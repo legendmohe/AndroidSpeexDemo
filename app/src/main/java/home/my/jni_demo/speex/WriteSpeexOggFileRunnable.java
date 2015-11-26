8eb0b0f84d0ee8ce61a8c4c4304f0873fdf37e79
Xinyu He <legendmohe@foxmail.com>
2015-11-26 11:07:19 +0800

    Update WriteSpeexOggFileRunnable.java
    fix WriteSpeexOggFileRunnable cannot exit

--- app/src/main/java/home/my/jni_demo/speex/WriteSpeexOggFileRunnable.java
+++ app/src/main/java/home/my/jni_demo/speex/WriteSpeexOggFileRunnable.java
@@ -7,46 +7,47 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class WriteSpeexOggFileRunnable implements Runnable {
-	private final static String TAG = "WriteSpeexOggFileRunnable";
-	private final Object mutex = new Object();
+    private final static String TAG = "WriteSpeexOggFileRunnable";
+    
     private final File mFile;
-
     private SpeexWriteClient mSpeexWriteClient = new SpeexWriteClient();
-	private volatile boolean mIsRecording;
-	private processedData mData;
-	private LinkedBlockingQueue<processedData> mDataQueue;
+    private volatile boolean mIsRecording;
+    private processedData mData;
+    private LinkedBlockingQueue<processedData> mDataQueue;
 
-	public static int write_packageSize = 1024;
+    public static int sWritePackageSize = 1024;
 
-	public WriteSpeexOggFileRunnable(File file) {
+    public WriteSpeexOggFileRunnable(File file) {
         this.mFile = file;
-		mDataQueue = new LinkedBlockingQueue<>();
-		mSpeexWriteClient.start(file, SpeexWriteClient.MODE_NB, SpeexWriteClient.SAMPLERATE_8000, true);
-	}
+        mDataQueue = new LinkedBlockingQueue<>();
+        mSpeexWriteClient.start(file, SpeexWriteClient.MODE_NB, SpeexWriteClient.SAMPLERATE_8000, true);
+    }
 
-	public void run() {
-		Log.d(TAG, "write thread runing");
+    public void run() {
+        Log.d(TAG, "write thread runing");
 
         mIsRecording = true;
-		while (this.isRecording()) {
+        while (this.isRecording()) {
             try {
                 mData = mDataQueue.take();
             } catch (InterruptedException e) {
                 Log.e(TAG, Log.getStackTraceString(e));
                 mData = null;
             }
-            if (mData != null) {
+            if (mData == sStopFlagData) {
+                mIsRecording = false;
+            }else if (mData != null) {
                 Log.d(TAG, "mData size=" + mData.size);
                 mSpeexWriteClient.writePacket(mData.processed, mData.size);
             }
-		}
+        }
         mSpeexWriteClient.stop();
 
         Log.d(TAG, "write thread exit");
-	}
+    }
 
-	public boolean putData(final byte[] buf, int size) {
-		processedData data = new processedData(buf, size);
+    public boolean putData(final byte[] buf, int size) {
+        processedData data = new processedData(buf, size);
         try {
             mDataQueue.put(data);
         } catch (InterruptedException e) {
@@ -56,29 +57,31 @@ public class WriteSpeexOggFileRunnable implements Runnable {
         return true;
     }
 
-	public void stop() {
-        synchronized (mutex) {
-            mIsRecording = false;
+    public void stop() {
+        try {
+            mDataQueue.put(sStopFlagData);
+        } catch (InterruptedException e) {
+            Log.e(TAG, Log.getStackTraceString(e));
         }
-	}
+    }
 
     public File getOutputFile() {
         return this.mFile;
     }
 
-	public boolean isRecording() {
-        synchronized (mutex) {
-            return mIsRecording;
-        }
-	}
+    public boolean isRecording() {
+        return mIsRecording;
+    }
 
-	class processedData {
+    class processedData {
         processedData(byte[] buf, int size) {
-            System.arraycopy(buf, 0, this.processed, 0, size);
+            if(buf != null)
+                System.arraycopy(buf, 0, this.processed, 0, size);
             this.size = size;
         }
-		private int size;
-		private byte[] processed = new byte[write_packageSize];
-	}
-
+        private int size;
+        private byte[] processed = new byte[sWritePackageSize];
+    }
+    
+    private static final ProcessedData sStopFlagData = new ProcessedData(null, -1);
 }
